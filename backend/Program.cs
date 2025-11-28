828b2b4c45fc159d837a2696a847b1bdb8a67110
ricky-lhy <rickylhy277@gmail.com>
2025-11-28 21:12:29 +0800

    (fix) enable swagger on non-development environment

--- backend/Program.cs
+++ backend/Program.cs
@@ -4,9 +4,6 @@ using Google.Api.Gax.Grpc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,17 +17,14 @@ builder.Services.AddSingleton<IRoutingService, GoogleRoutingService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "ExploreHKMO API");
-        options.RoutePrefix = "docs"; 
-    });
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "ExploreHKMO API");
+    options.RoutePrefix = "docs"; 
+});
+
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
